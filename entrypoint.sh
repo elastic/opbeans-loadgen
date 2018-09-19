819667132623e483b4f123b2a2f387e42cb07cb5
Dale McDiarmid <dalem@elastic.co>
2018-09-19 19:09:25 +0100

    Fix param order

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf Procfile
-python3 generate_procfile.py "$OPBEANS_URLS" "$OPBEANS_RPMS" "$OPBEANS_RLS" "$NUM_WOKERS" "$REAL_IP_HEADER" "$NUM_OF_IPS" > Procfile
+python3 generate_procfile.py "$OPBEANS_URLS" "$OPBEANS_RPMS" "$OPBEANS_RLS" "$NUM_WOKERS" "$NUM_OF_IPS" "$REAL_IP_HEADER"  > Procfile
 
 exec "$@"
