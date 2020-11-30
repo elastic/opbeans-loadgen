65047431a769c6ba6e6941c12c4194985ec6d173
Mike Place <mike.place@elastic.co>
2020-11-30 14:37:35 +0100

    CORS for the api as well

--- dyno/app/__init__.py
+++ dyno/app/__init__.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask_cors import CORS
 from app.cfg import Config as Cfg
 
 
 def create_app(config_class=Cfg):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    CORS(app)
     return app
 
 
 app = create_app()
 socketio = SocketIO(app, cors_allowed_origins='http://localhost:9000')
+
 
 from app.api import bp as api_bp  # noqa E402
 app.register_blueprint(api_bp, url_prefix='/api')
