e53de8dd8c00a785d5dfebd6960c1395a7fae69c
Mike Place <mike.place@elastic.co>
2021-01-07 13:05:25 +0100

    Fix logic error in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@
 rm -rf Procfile
 python3 generate_procfile.py "$OPBEANS_URLS" "$OPBEANS_RPMS" "$OPBEANS_RLS" > Procfile
 
-if [ -z $WS ];
+if [ -z ${WS+x}  ];
 then
+    echo "Starting load tests..."
+    exec "$@"
+else
     echo "Starting webserver..."
     cd dyno
     # TODO This should eventually be replaced with a proper application packaging strategy
     # but it works for the time being.
     export PYTHONPATH=$PYTHONPATH:/app
     gunicorn -b 0.0.0.0 --worker-class eventlet -w 1 app:app
-else
-    echo "Starting load tests..."
-    exec "$@"
 fi
